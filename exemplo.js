3d7b88d9ff44be197aa12c4b8d96731f44ffdbbf
Rafael Scorfi Gatto <rscorfi@gmail.com>
2025-06-02 21:27:03 -0300

    add exemplo

---
+++ exemplo.js
@@ -0,0 +1,22 @@
+let idade = 18
+if (idade >= 18){
+    console.log("Você é maior de idade!")
+} else {
+    console.log("Você é menor de idade!")
+}
+console.log("Fim")
+
+let nota = 98
+
+if (nota >= 60){
+    console.log ("Aprovado!")
+    if (nota >= 90){
+        console.log("Excelente")
+    } else if (nota >= 75){
+        console.log("Bom desempenho")
+    } else {
+        console.log("Pode melhorar")
+    }
+} else {
+    console.log("Reprovado")
+}
